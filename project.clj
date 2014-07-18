eacdec594cc2988bbc16e194f5479efc95874549
Sean Omlor <somlor@eml.cc>
2014-07-17 22:57:21 -0400

    hello world

--- project.clj
+++ project.clj
@@ -1,6 +1,11 @@
 (defproject hello "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :min-lein-version "2.0.0"
+  :description "First Clojure web app"
+  :url "ttp://still-sierra-6479.herokuapp.com"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [ring "1.3.0"]
+                 [compojure "1.1.8"]
+                 [de.ubercode.clostache/clostache "1.4.0"]]
+  :main hello.core)
