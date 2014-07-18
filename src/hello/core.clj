ac9af226d4bfd2e7aaebf950bcef08cc0fa47700
Sean Omlor <somlor@eml.cc>
2014-07-17 23:14:21 -0400

    mustache template functionality, index template

--- src/hello/core.clj
+++ src/hello/core.clj
@@ -1,10 +1,21 @@
 (ns hello.core
   (:use compojure.core)
   (:require
-    [ring.adapter.jetty :as jetty]))
+    [ring.adapter.jetty :as jetty]
+    [clostache.parser :as clostache]))
+
+(defn read-template [template-name]
+  (slurp (clojure.java.io/resource
+    (str "templates/" template-name ".mustache"))))
+
+(defn render-template [template-name params]
+  (clostache/render (read-template template-name) params))
+
+(defn index []
+  (render-template "index" {:greeting "Bonjour"}))
 
 (defroutes main-routes
-  (GET "/" [] "hello world"))
+  (GET "/" [] (index)))
 
 (defn -main []
   (jetty/run-jetty main-routes {:port 5000}))
