276c4ebd69485a46ff817240b9b5f93c9c9813ba
Sean Omlor <somlor@eml.cc>
2014-07-17 23:28:08 -0400

    get http port from env (for heroku)
    or default to 5000

--- src/hello/core.clj
+++ src/hello/core.clj
@@ -21,4 +21,5 @@
   (route/not-found "404 Not Found"))
 
 (defn -main []
-  (jetty/run-jetty main-routes {:port 5000}))
+  (let [port (Integer/parseInt (get (System/getenv) "PORT" "5000"))]
+    (jetty/run-jetty main-routes {:port port})))
